ba2c2cc3d3125128849aacb5b23bc6e725984834
daryl <livesaex@gmail.com>
2024-01-26 17:08:35 +0800

    feat: update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from 'react'
+import styles from './Home.module.scss'
+import { useNavigate } from 'react-router-dom'
+import { MANAGE_LIST } from '../router'
+import { Button, Typography } from 'antd'
+const { Title, Paragraph } = Typography
 
 const Home: FC = () => {
-  return <p>Home</p>
+  const nav = useNavigate()
+  return (
+    <div className={styles.container}>
+      <Title>问卷 | 调查 | 投票</Title>
+      <Paragraph>探索互联网无尽可能性，成为数字时代的建设者!</Paragraph>
+      <div className={styles.info}>
+        <Button type="default" onClick={() => nav(MANAGE_LIST)}>
+          开始使用
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Home
