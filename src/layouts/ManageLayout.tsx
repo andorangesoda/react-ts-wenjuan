dd4d5e619773748ae57a11c6d9aac7ab166ad236
daryl <livesaex@gmail.com>
2024-01-26 14:20:43 +0800

    feat: replace with antd

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,22 +1,45 @@
 import React, { FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import styles from './ManageLayout.module.scss'
+import { Button, Space } from 'antd'
+import { PlusCircleTwoTone, FileTextTwoTone, StarTwoTone, DeleteTwoTone } from '@ant-design/icons'
 
 const ManageLayout: FC = () => {
+  const nav = useNavigate()
+  const { pathname } = useLocation()
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <p>ManageLayout navigation</p>
-        <button>创建问卷</button>
-        <p>
-          <a href="#">我的问卷</a>
-        </p>
-        <p>
-          <a href="#">星标问卷</a>
-        </p>
-        <p>
-          <a href="#">回收站</a>
-        </p>
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusCircleTwoTone />}>
+            创建问卷
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/list') ? 'default' : 'link'}
+            size="large"
+            icon={<FileTextTwoTone />}
+            onClick={() => nav('/manage/list')}
+          >
+            我的问卷
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/star') ? 'default' : 'link'}
+            size="large"
+            icon={<StarTwoTone />}
+            onClick={() => nav('/manage/star')}
+          >
+            星标问卷
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/trash') ? 'default' : 'link'}
+            size="large"
+            icon={<DeleteTwoTone />}
+            onClick={() => nav('/manage/trash')}
+          >
+            回收站
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
